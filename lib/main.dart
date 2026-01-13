4a0ebfea0612a2959e39168b8d5cd89fe575cd84
Patryk <info@warriors-crew.co.uk>
2026-01-13 12:58:22 +0100

    .

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:adv_basics_app/startpage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 93, 6, 245),
+        body: StartPage(),
+      ),
+    ),
+  );
+}
